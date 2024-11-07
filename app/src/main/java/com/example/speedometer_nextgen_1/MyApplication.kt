5d94ac0c1d16ec8b3bb1494e09f190d378318caa
Brombeerkralle <80818631+Brombeerkralle@users.noreply.github.com>
2024-11-07 21:04:59 +0100

    added Hilt file for DI

---
+++ app/src/main/java/com/example/speedometer_nextgen_1/MyApplication.kt
@@ -0,0 +1,54 @@
+package com.example.speedometer_nextgen_1
+
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import android.view.View
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@HiltAndroidApp
+class MyApplication : Application()
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(@ApplicationContext context: Context): SharedPreferences {
+        return context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+    }
+
+    @Provides
+    fun provideMediaPlayerPlus(
+        @ApplicationContext context: Context,
+        sharedPreferences: SharedPreferences
+    ): MediaPlayerPlus {
+        val initialVolume = sharedPreferences.getFloat("backgroundVolume", 0.01f)
+        return MediaPlayerPlus(context, initialVolume)
+    }
+
+    @Provides
+    fun provideVolumeControlManager(
+        @ApplicationContext context: Context,
+        mediaPlayerPlus: MediaPlayerPlus,
+        sharedPreferences: SharedPreferences
+    ): VolumeControlManager {
+        val initialVolume = sharedPreferences.getFloat("backgroundVolume", 0.01f)
+        return VolumeControlManager(context, mediaPlayerPlus, sharedPreferences, initialVolume)
+    }
+
+    @Provides
+    fun provideSpeedManagement(
+        @ApplicationContext context: Context,
+        view: View
+    ): SpeedManagement {
+        return SpeedManagement(context, view)
+    }
+}
