da2d4cb1e1f463570986322988cbd799bea697c5
Brombeerkralle <80818631+Brombeerkralle@users.noreply.github.com>
2025-03-15 15:43:33 +0100

    Merge Dev to Main

--- app/src/main/java/com/example/speedometer_nextgen_1/MainActivity.kt
+++ app/src/main/java/com/example/speedometer_nextgen_1/MainActivity.kt
@@ -204,6 +204,7 @@ class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks, E
     }
 
     override fun onDestroy() {
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show()
         super.onDestroy()
         // Stop LocationService
         val locationServiceIntent = Intent(this, LocationService::class.java)
